c50c0c534424ab6b0cd53cbc77e1c476a8adbd62
wall paladino <wallevan@gmail.com>
2024-05-27 21:28:25 -0300

    segunda alteracao

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta !");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu Saldo !");
+        saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,sua agência é "+ agencia +", conta "
+        + numero +" e seu saldo "+ saldo +" já está disponível para saque");
+
+
+
+
     }
 }
